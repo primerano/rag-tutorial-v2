e9552445f4cfd561518dc5a82f8923f1f57d1595
Tony Primerano <github@tonycode.com>
2024-05-09 09:55:08 -0400

    use langchain_community, handle some deprecations and use Ollama phi3/nomic-embed-text for completely local rag

--- query_data.py
+++ query_data.py
@@ -1,5 +1,5 @@
 import argparse
-from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores.chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 
@@ -31,7 +31,6 @@ def query_rag(query_text: str):
     # Prepare the DB.
     embedding_function = get_embedding_function()
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
-
     # Search the DB.
     results = db.similarity_search_with_score(query_text, k=5)
 
@@ -40,7 +39,8 @@ def query_rag(query_text: str):
     prompt = prompt_template.format(context=context_text, question=query_text)
     # print(prompt)
 
-    model = Ollama(model="mistral")
+    #model = Ollama(model="mistral")
+    model = Ollama(model="phi3")
     response_text = model.invoke(prompt)
 
     sources = [doc.metadata.get("id", None) for doc, _score in results]
